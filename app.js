e731434a0f4ac57de93f80a57e4af6c9ad077fcb
avi-edgeware <avi.michael@edgewaretech.com.au>
2023-10-06 12:56:16 +1100

    bugs abound

--- app.js
+++ app.js
@@ -1,60 +1,70 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
-const utils = require('./utils.js')
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs").promises;
+const { DateTime } = require("luxon");
+const path = require("path");
+const utils = require("./utils.js");
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.use(bodyParser.json());
+// Endpoint to save timestamp
+app.post("/saveTimestamp", async (req, res) => {
+  const providedTimestamp = req.body.timestamp;
+  console.log(req.body.timestamp);
 
-app.use(express.static('public'));
+  // // Check if the provided timestamp is a valid UTC string
+  // const parsedTimestamp = DateTime.fromISO(providedTimestamp);
+  // if (!parsedTimestamp.isValid) {
+  //   return res.status(400).json({ error: "Invalid timestamp format" });
+  // }
 
-// Saves UTC timestamp to a file
-app.get('/saveTimestamp', async (req, res) => {
-  const timestamp = new Date().toUTCString();
-  const filePath = path.join(__dirname, 'timestamps.txt');
+  const filePath = path.join(__dirname, "timestamps.txt");
 
   try {
-    await fs.appendFile(filePath, timestamp + '\n');
-    console.log('Timestamp saved:', timestamp);
-    res.json({ timestamp });
+    // Use Luxon to get the timestamp in the desired format
+    const timestampToSave = DateTime.utc().toMillis().toString();
+
+    await fs.appendFile(filePath, timestampToSave + "\n");
+    console.log("Timestamp saved:", timestampToSave);
+    res.json({ timestamp: timestampToSave });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error saving timestamp to file');
+    res.status(500).send("Error saving timestamp to file");
   }
 });
 
-
 // Returns duration as a numerical value
-app.get('/getAccumulatedDuration', async (req, res) => {
-  const filePath = path.join(__dirname, 'accumulatedDuration.txt');
+app.get("/getAccumulatedDuration", async (req, res) => {
+  const filePath = path.join(__dirname, "accumulatedDuration.txt");
 
   try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    const timestamps = data.trim().split('\n');
+    const data = await fs.readFile(filePath, "utf-8");
+    const timestamps = data.trim().split("\n");
     const latestTimestamp = timestamps[timestamps.length - 1];
 
     res.json({ timestamp: latestTimestamp });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error fetching timestamp from file');
+    res.status(500).send("Error fetching timestamp from file");
   }
 });
 
-
 // Calculates duration between two UTC timestamps and saves duration to file
-app.get('/saveAccumulatedDuration', async (req, res) => {
+app.get("/saveAccumulatedDuration", async (req, res) => {
   const timestampNow = new Date().toUTCString();
-  const filePath = path.join(__dirname, 'timestamps.txt');
+  const filePath = path.join(__dirname, "timestamps.txt");
   let previousTimestamp;
 
   try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    const timestamps = data.trim().split('\n');
+    const data = await fs.readFile(filePath, "utf-8");
+    const timestamps = data.trim().split("\n");
     previousTimestamp = timestamps[timestamps.length - 1];
   } catch (err) {
     console.error(err);
-    return res.status(500).send('Error fetching timestamp from file');
+    return res.status(500).send("Error fetching timestamp from file");
   }
 
   const previous = new Date(previousTimestamp);
@@ -63,48 +73,70 @@ app.get('/saveAccumulatedDuration', async (req, res) => {
   // Calculate the time difference in milliseconds
   const timeDifference = now - previous;
 
-  const accumulatedDurationFilePath = path.join(__dirname, 'accumulatedDuration.txt');
+  const accumulatedDurationFilePath = path.join(
+    __dirname,
+    "accumulatedDuration.txt"
+  );
 
   try {
-    await fs.appendFile(accumulatedDurationFilePath, timeDifference + '\n');
-    console.log('Timestamp duration:', timeDifference);
+    await fs.appendFile(accumulatedDurationFilePath, timeDifference + "\n");
+    console.log("Timestamp duration:", timeDifference);
     res.json({ timeDifference });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error saving timestamp to file');
+    res.status(500).send("Error saving timestamp to file");
   }
 });
 
 // Return timestamp
-app.get('/getTimestamp', async (req, res) => {
+app.get("/getTimestamp", async (req, res) => {
   const timestamp = await utils.getLatestTimestamp();
   console.log(timestamp);
   res.json({ timestamp });
 });
 
-// Pause button: store the previously accumulated duration to a different file, 
-// and check to see if that duration is empty prior to displaying time.
-// If a person returns to the site and the accumlated duration file has a value, display that
-// if user clicks start, then continue counting up from the current time + the accumulated duration
-app.get('/updateTimestamp', async (req, res) => {
-  const filePath = path.join(__dirname, 'timestamps.txt');
-
+// Wipe timestamps file
+app.get("/clearTimestamp", async (req, res) => {
   try {
-    await fs.appendFile(filePath, timestamp + '\n');
-    console.log('Timestamp saved:', timestamp);
-    res.json({ timestamp });
+    await utils.clearFile("timestamps.txt");
+    res.json({ response: "success" });
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error saving timestamp to file');
+    res.status(500).send("Error saving clearing timestamps file");
   }
 });
 
+// Wipe timestamps file
+app.get("/clearAccumulatedDuration", (req, res) => {
+  try {
+    utils.clearFile("accumulatedDuration.txt");
+    res.json({ response: "success" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving clearing timestamps file");
+  }
+});
 
+// Pause button: store the previously accumulated duration to a different file,
+// and check to see if that duration is empty prior to displaying time.
+// If a person returns to the site and the accumlated duration file has a value, display that
+// if user clicks start, then continue counting up from the current time + the accumulated duration
+app.get("/updateTimestamp", async (req, res) => {
+  const filePath = path.join(__dirname, "timestamps.txt");
 
+  try {
+    await fs.appendFile(filePath, timestamp + "\n");
+    console.log("Timestamp saved:", timestamp);
+    res.json({ timestamp });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error saving timestamp to file");
+  }
+});
 
 // Serve the HTML file at the root endpoint
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Start the server
--- utils.js
+++ utils.js
@@ -1,49 +1,62 @@
-
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require("fs").promises;
+const path = require("path");
 
 const { rejects } = require("assert");
 
-exports.getAdjustedTimeStamp = function() {
-    return new Promise( async (resolve, reject) => {
-        try {
-            const latestTimestamp = await getLatestTimestamp();
-            const existingDuration = await getAdjustment();
-            const adjustedTimeStamp = latestTimestamp + existingDuration;
-            resolve(adjustedTimeStamp);
+exports.getAdjustedTimeStamp = function () {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const latestTimestamp = await getLatestTimestamp();
+      const existingDuration = await getAdjustment();
+      const adjustedTimeStamp = latestTimestamp + existingDuration;
+      resolve(adjustedTimeStamp);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
+exports.getLatestTimestamp = function () {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const filePath = path.join(__dirname, "timestamps.txt");
+      const data = await fs.readFile(filePath, "utf-8");
+      const timestamps = data.trim().split("\n");
+      const latestTimestamp = timestamps[timestamps.length - 1];
+      console.log("getting timestamp");
+      console.log(latestTimestamp);
+      resolve(latestTimestamp);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-  } catch (err) {
-    reject(err);
-  }
-    })
-}
+exports.getAdjustment = function () {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const filePath = path.join(__dirname, "accumulatedDuration.txt");
+      const data = await fs.readFile(filePath, "utf-8");
+      const timestamps = data.trim().split("\n");
+      const latestTimestamp = timestamps[timestamps.length - 1];
+      resolve(latestTimestamp);
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-exports.getLatestTimestamp = function() {
-    return new Promise( async (resolve, reject) => {
-        try {
-        const filePath = path.join(__dirname, 'timestamps.txt');
-        const data = await fs.readFile(filePath, 'utf-8');
-        const timestamps = data.trim().split('\n');
-        const latestTimestamp = timestamps[timestamps.length - 1];
-    resolve(latestTimestamp)
-  } catch (err) {
-    reject(err);
-  }
-    })
-}
-
-
-exports.getAdjustment = function() {
-    return new Promise( async (resolve, reject) => {
-        try {
-        const filePath = path.join(__dirname, 'accumulatedDuration.txt');
-        const data = await fs.readFile(filePath, 'utf-8');
-        const timestamps = data.trim().split('\n');
-        const latestTimestamp = timestamps[timestamps.length - 1];
-    resolve(latestTimestamp)
-  } catch (err) {
-    reject(err);
-  }
-    })
-}
+exports.clearFile = function (filePath) {
+  return new Promise(async (resolve, reject) => {
+    console.log("clearing files");
+    fs.writeFile(filePath, "", (err) => {
+      if (err) {
+        console.error("Error deleting lines in the file:", err);
+        reject(err);
+      } else {
+        console.log("All lines deleted successfully.");
+        resolve(filePath);
+      }
+    });
+  });
+};
